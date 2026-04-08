2a3f721b3a588f157156da37e8370a2917fa5113
hashumshum <55711149+hashumshum@users.noreply.github.com>
2026-04-08 08:16:53 +0300

    todo 4

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -130,6 +130,14 @@ app.get("/profile/:first/:last", (req, res) => {
 
 
 // Route param middleware example: /users/42
+app.param("userId", (req, res, next, userId) => {
+  const num = Number(userId);
+  if (isNaN(num) || num <= 0) {
+    return res.status(400).json({ ok: false, error: "userId must be positive number" });
+  }
+  req.userIdNum = num;
+  next();
+});
 
 
 // Route params: /users/:userId route
